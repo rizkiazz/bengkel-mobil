002de4ffbffb9906c2381dfeaca4acf9ec2351c1
unknown <rizkiaziz124@gmail.com>
2023-07-26 13:36:35 +0700

    update view layout

--- resources/views/layouts/admin/leftsidebar.blade.php
+++ resources/views/layouts/admin/leftsidebar.blade.php
@@ -7,50 +7,15 @@
         <!-- Sidebar navigation-->
         <nav class="sidebar-nav">
             <ul id="sidebarnav">
-                <li class="nav-small-cap ml-3"><h4>PERSONAL</h4></li>
                 <li> <a class="waves-effect waves-dark" href="{{ url('admin/dashboard') }}"><i class="mdi mdi-view-dashboard"></i><span class="hide-menu">Dashboard</a></li>
+                <li class="nav-small-cap ml-3"><h4>Kendaraan</h4></li>
                 <li> <a class="waves-effect waves-dark" href="{{ url('admin/kategori') }}"><i class="mdi mdi-view-dashboard"></i><span class="hide-menu">Kategori</a></li>
                 <li> <a class="waves-effect waves-dark" href="{{ url('admin/kendaraan') }}"><i class="mdi mdi-view-dashboard"></i><span class="hide-menu">Kendaraan</a></li>
                 <li> <a class="waves-effect waves-dark" href="{{ url('admin/suku-cadang') }}"><i class="mdi mdi-view-dashboard"></i><span class="hide-menu">Suku Cadang</a></li>
                 <li> <a class="waves-effect waves-dark" href="{{ url('admin/supplier') }}"><i class="mdi mdi-view-dashboard"></i><span class="hide-menu">Supplier</a></li>
                 <li> <a class="waves-effect waves-dark" href="{{ url('admin/pelanggan') }}"><i class="mdi mdi-view-dashboard"></i><span class="hide-menu">Pelanggan</a></li>
                 <li> <a class="waves-effect waves-dark" href="{{ url('admin/service') }}"><i class="mdi mdi-view-dashboard"></i><span class="hide-menu">Service</a></li>
-                {{-- <li> <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="icon-speedometer"></i><span class="hide-menu">Kategori <span class="badge badge-pill badge-cyan ml-auto">1</span></span></a>
-                    <ul aria-expanded="false" class="collapse">
-                        <li><a href="{{ url('admin/kategori/create') }}">Tambah Kategori </a></li>
-                        <li><a href="{{ url('admin/kategori') }}">Daftar Kategori </a></li>
-                    </ul>
-                </li>
-                <li> <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="icon-speedometer"></i><span class="hide-menu">Kendaraan <span class="badge badge-pill badge-cyan ml-auto">1</span></span></a>
-                    <ul aria-expanded="false" class="collapse">
-                        <li><a href="{{ url('admin/kendaraan/create') }}">Tambah Kendaraan </a></li>
-                        <li><a href="{{ url('admin/kendaraan') }}">Daftar Kendaraan </a></li>
-                    </ul>
-                </li>
-                <li> <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="icon-speedometer"></i><span class="hide-menu">Suku Cadang <span class="badge badge-pill badge-cyan ml-auto">1</span></span></a>
-                    <ul aria-expanded="false" class="collapse">
-                        <li><a href="{{ url('admin/suku-cadang/create') }}">Tambah Suku Cadang </a></li>
-                        <li><a href="{{ url('admin/suku-cadang') }}">Daftar Suku Cadang </a></li>
-                    </ul>
-                </li>
-                <li> <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="icon-speedometer"></i><span class="hide-menu">Service <span class="badge badge-pill badge-cyan ml-auto">1</span></span></a>
-                    <ul aria-expanded="false" class="collapse">
-                        <li><a href="{{ url('admin/service/create') }}">Tambah Service </a></li>
-                        <li><a href="{{ url('admin/service') }}">Daftar Service </a></li>
-                    </ul>
-                </li>
-                <li> <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="icon-speedometer"></i><span class="hide-menu">Supplier <span class="badge badge-pill badge-cyan ml-auto">1</span></span></a>
-                    <ul aria-expanded="false" class="collapse">
-                        <li><a href="{{ url('admin/supplier/create') }}">Tambah Supplier </a></li>
-                        <li><a href="{{ url('admin/supplier') }}">Daftar Supplier </a></li>
-                    </ul>
-                </li>
-                <li> <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="icon-speedometer"></i><span class="hide-menu">Pelanggan <span class="badge badge-pill badge-cyan ml-auto">1</span></span></a>
-                    <ul aria-expanded="false" class="collapse">
-                        <li><a href="{{ url('admin/pelanggan/create') }}">Tambah Pelanggan </a></li>
-                        <li><a href="{{ url('admin/pelanggan') }}">Daftar Pelanggan </a></li>
-                    </ul>
-                </li>  --}}
+                <li class="nav-small-cap ml-3"><h4>Transaksi</h4></li>
                 <li> <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="icon-speedometer"></i><span class="hide-menu">Pembayaran <span class="badge badge-pill badge-cyan ml-auto">1</span></span></a>
                     <ul aria-expanded="false" class="collapse">
                         <li><a href="{{ url('admin/pembayaran/create') }}">Tambah Pembayaran </a></li>
@@ -69,6 +34,7 @@
                         <li><a href="{{ url('admin/pengeluaran') }}">Daftar Pengeluaran </a></li>
                     </ul>
                 </li>
+                <li class="nav-small-cap ml-3"><h4>Manajemen Karyawan</h4></li>
                 <li> <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="icon-speedometer"></i><span class="hide-menu">Users <span class="badge badge-pill badge-cyan ml-auto">1</span></span></a>
                     <ul aria-expanded="false" class="collapse">
                         <li><a href="{{ url('admin/users/create') }}">Tambah Users </a></li>
@@ -81,6 +47,7 @@
                         <li><a href="{{ url('admin/karyawan') }}">Daftar Karyawan </a></li>
                     </ul>
                 </li>
+                <li class="nav-small-cap ml-3"><h4>Laporan</h4></li>
                 <li> <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="icon-speedometer"></i><span class="hide-menu">Laporan <span class="badge badge-pill badge-cyan ml-auto">1</span></span></a>
                     <ul aria-expanded="false" class="collapse">
                         <li><a href="{{ url('admin/laporan/create') }}">Tambah Laporan </a></li>
